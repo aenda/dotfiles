5aafdf07f0fcc5632681a631a150195691ebdbcb
Genevieve Mendoza <aenda@users.noreply.github.com>
2017-12-09 12:31:22 -0600

    bug fixes in color handling by i3, zsh, script

--- changecolor.sh
+++ changecolor.sh
@@ -4,14 +4,12 @@ export colormode=$1
 
 if [ $colormode = "light" ]
 then
-    rm /home/gmend/.dotfiles/nvim/background.vim
-    echo "\"set background=dark" >> /home/gmend/.dotfiles/nvim/background.vim
+    echo "\"set background=dark" > /home/gmend/.dotfiles/nvim/background.vim
     ln -sfv /home/gmend/.dotfiles/termite/config-gruvbox-light /home/gmend/.config/termite/config
     ln -sfv /home/gmend/.dotfiles/nvim/color-light.vim /home/gmend/.config/nvim/color.vim
 elif [ $colormode = "dark" ]
 then
-    rm /home/gmend/.dotfiles/nvim/background.vim
-    echo "set background=dark" >> /home/gmend/.dotfiles/nvim/background.vim
+    echo "set background=dark" > /home/gmend/.dotfiles/nvim/background.vim
     ln -sfv /home/gmend/.dotfiles/termite/config-gruvbox-dark /home/gmend/.config/termite/config
     ln -sfv /home/gmend/.dotfiles/nvim/color-dark.vim /home/gmend/.config/nvim/color.vim
 else

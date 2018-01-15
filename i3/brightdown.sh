88bf296a7e9b2f9c6ac900739df73afe099a8086
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-01-15 15:56:01 -0500

    will never echo invalid (ie negative) number to backlight brightness file

--- i3/brightdown.sh
+++ i3/brightdown.sh
@@ -2,4 +2,8 @@
 
 read -r level < /sys/class/backlight/intel_backlight/actual_brightness
 newlevel=`expr $level - 100`
-echo $newlevel >> /sys/class/backlight/intel_backlight/brightness
+#echo $newlevel
+#echo $(expr $newlevel - 100 \> 0) 
+if [ $newlevel -gt 0 ]; then
+  echo $newlevel >> /sys/class/backlight/intel_backlight/brightness
+fi

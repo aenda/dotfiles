9f33870ddafb52df5259dd965f60aa4781c3e7cf
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-04-28 19:22:05 -0500

    speed up screen lock

--- scripts/screen-lock.sh
+++ scripts/screen-lock.sh
@@ -4,10 +4,11 @@
 if [ -z "$XDG_RUNTIME_DIR" ] && [ -z "$SWAYSOCK"]; then
     uid=$(id -u $USER)
     export XDG_RUNTIME_DIR="/run/user/"$uid"/"
-    export SWAYSOCK=$(find $XDG_RUNTIME_DIR -iname sway*sock)
+    #export SWAYSOCK=$(find $XDG_RUNTIME_DIR -iname sway*sock)
+    export SWAYSOCK=$(sway --get-socketpath)
 fi
 
-swaygrab /home/$USER/.dotfiles/sway/lockblur.png
-convert -blur 0x6 /home/$USER/.dotfiles/sway/lockblur.png /home/$USER/.dotfiles/sway/lockblur.png
+#swaygrab /home/$USER/.dotfiles/sway/lockblur.png
+#convert -blur 0x6 /home/$USER/.dotfiles/sway/lockblur.png /home/$USER/.dotfiles/sway/lockblur.png
 
-swaylock -i /home/$USER/.dotfiles/sway/lockblur.png
+swaylock -i /home/$USER/.dotfiles/sway/lockscreen.png

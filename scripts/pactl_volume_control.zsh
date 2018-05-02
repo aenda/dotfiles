fcf6efbb561e1b046fab4754cab98bfe4018a11b
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-05-02 16:22:03 -0500

    check for headphones before increasing volume past 20%

--- scripts/pactl_volume_control.zsh
+++ scripts/pactl_volume_control.zsh
@@ -4,13 +4,20 @@ SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head 
 get_sink_vol() {
     pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
 }
+check_headphones() {
+    pacmd list-cards | grep Headphones
+}
 
 if [[ "$1" == "mute" ]]; then
     /usr/bin/pactl set-sink-mute "${$(pactl info | grep Sink)#*:\ }" toggle
 elif [[ "$1" == "up" ]]; then
-    if [[ "$(get_sink_vol)" -lt 30 ]]; then
-        exec /usr/bin/pactl set-sink-volume "${$(pactl info | grep Sink)#*:\ }" "+5%"
-    fi
+    if [[ "$(check_headphones)" == *yes* ]]; then
+        if [[ "$(get_sink_vol)" -lt 20 ]]; then
+            exec /usr/bin/pactl set-sink-volume "${$(pactl info | grep Sink)#*:\ }" "+5%";
+        fi;
+    else;
+        exec /usr/bin/pactl set-sink-volume "${$(pactl info | grep Sink)#*:\ }" "+5%";
+    fi;
 elif [[ "$1" == "down" ]]; then
     exec /usr/bin/pactl set-sink-volume "${$(pactl info | grep Sink)#*:\ }" "-5%"
 else

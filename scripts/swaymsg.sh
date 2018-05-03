74e44cb3c9b9cfd4e67f2314b2f7f2be79b8a0a8
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-05-03 13:43:54 -0500

    autostart spotify, update script dirs

--- scripts/swaymsg.sh
+++ scripts/swaymsg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swaymsg 'workspace number 2:term; exec /bin/kitty; workspace number 1:vim; exec /bin/kitty; workspace number 3:browse; exec env MOZ_USE_XINPUT2=1 firefox -profile /home/gmend/.config/mozilla/firefox/ && sleep 1 && rm -r /home/gmend/.mozilla; workspace number 4:misc; exec /bin/spotify; workspace number 1:vim;'
+swaymsg 'workspace number 2:term; exec /bin/kitty; workspace number 1:vim; exec /bin/kitty; workspace number 3:browse; exec env MOZ_USE_XINPUT2=1 firefox -profile /home/gmend/.config/mozilla/firefox/; exec sleep 1 && rm -r /home/gmend/.mozilla; workspace number 4:misc; exec spotify; exec sleep 10 && rm -r /home/gmend/.pki; workspace number 1:vim;'

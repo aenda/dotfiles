70497d742d643470968d4ea3ab5728f5627f9d0b
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-05-15 21:27:11 -0500

    support multiple sinks - control the currently running one with pactl

--- scripts/pactl_volume_control.zsh
+++ scripts/pactl_volume_control.zsh
@@ -1,6 +1,7 @@
 #!/bin/env zsh
 
-SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+#SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+SINK="$( pactl list short sinks | awk '/RUNNING/ {print $1}' )"
 get_sink_vol() {
     pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
 }

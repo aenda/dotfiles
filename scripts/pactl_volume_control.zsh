563c52f99377bad4468ee5739562abff9fc66157
Genevieve Mendoza <aenda@users.noreply.github.com>
2018-05-30 00:36:22 -0500

    misc

--- scripts/pactl_volume_control.zsh
+++ scripts/pactl_volume_control.zsh
@@ -1,7 +1,11 @@
 #!/bin/env zsh
 
-#SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
 SINK="$( pactl list short sinks | awk '/RUNNING/ {print $1}' )"
+if [[ -z "$SINK" ]]; then
+#   echo "the sink variable is empty"
+    SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+fi
+# TODO: if music is paused, it won't be running, so we should add a backup check here
 get_sink_vol() {
     pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
 }
